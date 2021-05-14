ecd00221b1192e1ecbbaa18993c4b4bb58b81a66
Кирилл <47419037+ForDeveloperMan@users.noreply.github.com>
2021-05-14 18:15:09 +0300

    fsgdghrty

--- app/js/main.js
+++ app/js/main.js
@@ -7,11 +7,46 @@ function unsetNoscroll(){
 	$('html').removeClass('noscroll');
 }
 
+function getScrollBarWidth (){
+  var inner = document.createElement('p');
+  inner.style.width = "100%";
+  inner.style.height = "200px";
+
+  var outer = document.createElement('div');
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "200px";
+  outer.style.height = "150px";
+  outer.style.overflow = "hidden";
+  outer.appendChild (inner);
+
+  document.body.appendChild (outer);
+  var w1 = inner.offsetWidth;
+  outer.style.overflow = 'scroll';
+  var w2 = inner.offsetWidth;
+  if (w1 == w2) w2 = outer.clientWidth;
+
+  document.body.removeChild(outer);
+
+  return (w1 - w2);
+};
+
+$('head').append('<style>.noscroll{margin-right: '+getScrollBarWidth()+'px;}</style>');
+
 $('.linkTo').on('click', function(){
 	var to = $(this).attr('href');
 	$('body, html').animate({
 		scrollTop: $(to).offset().top
 	}, 700);
+	return false;
+});
+
+$('.menu-dropdown-links__link').on('click', function(){
+	unsetNoscroll();
+	$('.menu-dropdown').removeClass('active');
+	$('.header__btn').removeClass('active');
 	return false;
 });
 
